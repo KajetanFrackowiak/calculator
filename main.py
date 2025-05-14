76735df8e64711ed65ce112f4b8234ed0e071a5f
KajetanFrackowiak <kajtek.gdynia@gmail.com>
2025-05-14 08:47:08 +0200

    .

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def add(a, b):
     return a + b
 
+
 def subtraction(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
+
 
 if __name__ == "__main__":  # pragma: no cover
     print(add(1, 2))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,13 @@
 from main import add, subtraction, multiply
 
+
 def test_add():
     assert add(1, 2) == 3
+
 
 def test_subtraction():
     assert subtraction(5, 3) == 2
 
-def test_multuply():
+
+def test_multiply():
     assert multiply(5, 3) == 15
