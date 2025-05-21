95c6578c34dddf57875a6390c86c216cd7b366b6
KajetanFrackowiak <kajtek.gdynia@gmail.com>
2025-05-21 12:42:20 +0200

    .

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ cache = hz_client.get_map("calc-cache").blocking()
 
 app = Flask(__name__)
 
+
 def cached_operation(op_name, a, b):
     key = f"{op_name}:{a}:{b}"
     if cache.contains_key(key):
@@ -24,11 +25,13 @@ def cached_operation(op_name, a, b):
         cache.put(key, result)
         return result
 
+
 @app.route("/add")
 def add_route():
     a = int(request.args.get("a", 0))
     b = int(request.args.get("b", 0))
     return jsonify({"result": cached_operation("add", a, b)})
+
 
 @app.route("/subtract")
 def subtract_route():
@@ -36,11 +39,13 @@ def subtract_route():
     b = int(request.args.get("b", 0))
     return jsonify({"result": cached_operation("subtract", a, b)})
 
+
 @app.route("/multiply")
 def multiply_route():
     a = int(request.args.get("a", 0))
     b = int(request.args.get("b", 0))
     return jsonify({"result": cached_operation("multiply", a, b)})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
